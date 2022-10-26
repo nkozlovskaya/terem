f36af5209f079d129c10088d66010778fc42570b
nadezhda <kozlovskayani@yandex.ru>
2022-10-26 12:01:52 +0300

    editing form js

--- js/form.js
+++ js/form.js
@@ -1,61 +1,60 @@
+// function sendJSON() {
+//   const name = document.querySelector("#name");
+//   const phone = document.querySelector("#phone");
+//   const select = document.querySelector("select");
+//   const result = document.querySelector(".result");
+//   const xhr = new XMLHttpRequest();
+//   const url = "http://127.0.0.1:5500/form.php";
+//   xhr.open("POST", url, true);
+//   xhr.setRequestHeader("Content-Type", "application/json");
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4 && xhr.status === 200) {
+//       result.innerHTML = this.responseText;
+//     }
+//   };
+//   const data = JSON.stringify({
+//     select: select.value,
+//     name: name.value,
+//     phone: phone.value,
+//   });
+//   xhr.send(data);
+// }
 
+const sendData = async (url, data) => {
+  const response = await fetch(url, {
+    method: "POST",
+    body: data,
+  });
 
-function sendJSON() {
+  if (!response.ok) {
+    throw new Error(
+      `Ошибка по адресу ${url}, статус ошибки ${response.status}`
+    );
+  }
+
+  return await response.json();
+};
+
+const sendForm = () => {
   const name = document.querySelector("#name");
   const phone = document.querySelector("#phone");
   const select = document.querySelector("select");
-  const result = document.querySelector(".result");
-  const xhr = new XMLHttpRequest();
-  const url = "http://127.0.0.1:5500/form.php";
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      result.innerHTML = this.responseText;
-    }
+  const cartForm = document.querySelector(".form");
+  const data = {
+    name,
+    phone,
+    select,
   };
-  var data = JSON.stringify({
-    select: select.value,
-    name: name.value,
-    phone: phone.value,
+  cartForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // const cartList = JSON.stringify(data);
+    const formData = new FormData(cartForm);
+    sendData("https://jsonplaceholder.typicode.com/posts", formData)
+      .then(() => {
+        cartForm.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
-  xhr.send(data);
-}
-// $(document).ready(function () {
-//   let btnForm = document.querySelector("#formBtn");
-
-// $(document).on("click", "#formBtn", function () {
-//   var name = document.querySelector("#name");
-//   var phone = document.querySelector("#phone");
-//   var result = document.querySelector(".result");
-//   $.ajax({
-//     url: "form.php",
-//     type: "POST",
-//     dataType: "json",
-//     data: {
-//       name: name,
-//       phone: phone,
-//     },
-//     success: function (data) {
-//       $(result).text("Имя пользователя:" + data.name, "телефон: " + data.phone);
-//     },
-//     error: function () {
-//       console.log("ERROR");
-//     },
-//   });
-// });
-// });
-// $(document).ready(function () {
-//   $("#formBtn").click(function () {
-//     $.post(
-//       "form.php",
-//       {
-//         name: name,
-//         phone: phone,
-//       },
-//       function (data) {
-//         alert("Имя пользователя: " + data.name + "телефон " + data.phone);
-//       }
-//     );
-//   });
-// });
+};
